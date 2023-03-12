70df150b7a37ac31bf913cc0770d68cf9c6caf17
Yaroslav Kolomoiets <tortocool@gmail.com>
2023-03-12 18:27:14 +0100

    start_adaptiv

---
+++ script.js
@@ -0,0 +1,247 @@
+$("#btn-1").on("click", function () {
+  $("#div-1").slideToggle();
+});
+$(function () {
+  $("#btn-1").on("click", function () {
+    if ($('p',this).hasClass("arrow")) {
+      $('p',this).removeClass("arrow").addClass("arrow-rotate");
+    } else if ($('p',this).hasClass("arrow-rotate")) {
+      $('p',this).removeClass("arrow-rotate").addClass("arrow");
+    }
+  });
+});
+
+
+
+$("#btn-2").on("click", function () {
+  $("#div-2").slideToggle();
+});
+$(function () {
+  $("#btn-2").on("click", function () {
+    if ($('p',this).hasClass("arrow")) {
+      $('p',this).removeClass("arrow").addClass("arrow-rotate");
+    } else if ($('p',this).hasClass("arrow-rotate")) {
+      $('p',this).removeClass("arrow-rotate").addClass("arrow");
+    }
+  });
+});
+
+$("#btn-3").on("click", function () {
+  $("#div-3").slideToggle();
+});
+$(function () {
+  $("#btn-3").on("click", function () {
+    if ($('p',this).hasClass("arrow")) {
+      $('p',this).removeClass("arrow").addClass("arrow-rotate");
+    } else if ($('p',this).hasClass("arrow-rotate")) {
+      $('p',this).removeClass("arrow-rotate").addClass("arrow");
+    }
+  });
+});
+
+$("#btn-4").on("click", function () {
+  $("#div-4").slideToggle();
+});
+$(function () {
+  $("#btn-4").on("click", function () {
+    if ($('p',this).hasClass("arrow")) {
+      $('p',this).removeClass("arrow").addClass("arrow-rotate");
+    } else if ($('p',this).hasClass("arrow-rotate")) {
+      $('p',this).removeClass("arrow-rotate").addClass("arrow");
+    }
+  });
+});
+
+$("#btn-5").on("click", function () {
+  $("#div-5").slideToggle();
+});
+$(function () {
+  $("#btn-5").on("click", function () {
+    if ($('p',this).hasClass("arrow")) {
+      $('p',this).removeClass("arrow").addClass("arrow-rotate");
+    } else if ($('p',this).hasClass("arrow-rotate")) {
+      $('p',this).removeClass("arrow-rotate").addClass("arrow");
+    }
+  });
+});
+
+$("#btn-6").on("click", function () {
+  $("#div-6").slideToggle();
+});
+$(function () {
+  $("#btn-6").on("click", function () {
+    if ($('p',this).hasClass("arrow")) {
+      $('p',this).removeClass("arrow").addClass("arrow-rotate");
+    } else if ($('p',this).hasClass("arrow-rotate")) {
+      $('p',this).removeClass("arrow-rotate").addClass("arrow");
+    }
+  });
+});
+
+$("#btn-7").on("click", function () {
+  $("#div-7").slideToggle();
+});
+$(function () {
+  $("#btn-7").on("click", function () {
+    if ($('p',this).hasClass("arrow")) {
+      $('p',this).removeClass("arrow").addClass("arrow-rotate");
+    } else if ($('p',this).hasClass("arrow-rotate")) {
+      $('p',this).removeClass("arrow-rotate").addClass("arrow");
+    }
+  });
+});
+
+$("#btn-8").on("click", function () {
+  $("#div-8").slideToggle();
+});
+$(function () {
+  $("#btn-8").on("click", function () {
+    if ($('p',this).hasClass("arrow")) {
+      $('p',this).removeClass("arrow").addClass("arrow-rotate");
+    } else if ($('p',this).hasClass("arrow-rotate")) {
+      $('p',this).removeClass("arrow-rotate").addClass("arrow");
+    }
+  });
+});
+
+$("#btn-9").on("click", function () {
+  $("#div-9").slideToggle();
+});
+$(function () {
+  $("#btn-9").on("click", function () {
+    if ($('p',this).hasClass("arrow")) {
+      $('p',this).removeClass("arrow").addClass("arrow-rotate");
+    } else if ($('p',this).hasClass("arrow-rotate")) {
+      $('p',this).removeClass("arrow-rotate").addClass("arrow");
+    }
+  });
+});
+
+$("#btn-10").on("click", function () {
+  $("#div-10").slideToggle();
+});
+$(function () {
+  $("#btn-10").on("click", function () {
+    if ($('p',this).hasClass("arrow")) {
+      $('p',this).removeClass("arrow").addClass("arrow-rotate");
+    } else if ($('p',this).hasClass("arrow-rotate")) {
+      $('p',this).removeClass("arrow-rotate").addClass("arrow");
+    }
+  });
+});
+$("#btn-11").on("click", function () {
+  $("#div-11").slideToggle();
+});
+$(function () {
+  $("#btn-11").on("click", function () {
+    if ($('p',this).hasClass("arrow")) {
+      $('p',this).removeClass("arrow").addClass("arrow-rotate");
+    } else if ($('p',this).hasClass("arrow-rotate")) {
+      $('p',this).removeClass("arrow-rotate").addClass("arrow");
+    }
+  });
+});
+
+$("#btn-12").on("click", function () {
+  $("#div-12").slideToggle();
+});
+$(function () {
+  $("#btn-12").on("click", function () {
+    if ($('p',this).hasClass("arrow")) {
+      $('p',this).removeClass("arrow").addClass("arrow-rotate");
+    } else if ($('p',this).hasClass("arrow-rotate")) {
+      $('p',this).removeClass("arrow-rotate").addClass("arrow");
+    }
+  });
+});
+
+$("#btn-13").on("click", function () {
+  $("#div-13").slideToggle();
+});
+$(function () {
+  $("#btn-13").on("click", function () {
+    if ($('p',this).hasClass("arrow")) {
+      $('p',this).removeClass("arrow").addClass("arrow-rotate");
+    } else if ($('p',this).hasClass("arrow-rotate")) {
+      $('p',this).removeClass("arrow-rotate").addClass("arrow");
+    }
+  });
+});
+
+$("#btn-14").on("click", function () {
+  $("#div-14").slideToggle();
+});
+$(function () {
+  $("#btn-14").on("click", function () {
+    if ($('p',this).hasClass("arrow")) {
+      $('p',this).removeClass("arrow").addClass("arrow-rotate");
+    } else if ($('p',this).hasClass("arrow-rotate")) {
+      $('p',this).removeClass("arrow-rotate").addClass("arrow");
+    }
+  });
+});
+
+$("#btn-15").on("click", function () {
+  $("#div-15").slideToggle();
+});
+$(function () {
+  $("#btn-15").on("click", function () {
+    if ($('p',this).hasClass("arrow")) {
+      $('p',this).removeClass("arrow").addClass("arrow-rotate");
+    } else if ($('p',this).hasClass("arrow-rotate")) {
+      $('p',this).removeClass("arrow-rotate").addClass("arrow");
+    }
+  });
+});
+
+$("#btn-16").on("click", function () {
+  $("#div-16").slideToggle();
+});
+$(function () {
+  $("#btn-16").on("click", function () {
+    if ($('p',this).hasClass("arrow")) {
+      $('p',this).removeClass("arrow").addClass("arrow-rotate");
+    } else if ($('p',this).hasClass("arrow-rotate")) {
+      $('p',this).removeClass("arrow-rotate").addClass("arrow");
+    }
+  });
+});
+
+$("#btn-17").on("click", function () {
+  $("#div-17").slideToggle();
+});
+$(function () {
+  $("#btn-17").on("click", function () {
+    if ($('p',this).hasClass("arrow")) {
+      $('p',this).removeClass("arrow").addClass("arrow-rotate");
+    } else if ($('p',this).hasClass("arrow-rotate")) {
+      $('p',this).removeClass("arrow-rotate").addClass("arrow");
+    }
+  });
+});
+
+$("#btn-18").on("click", function () {
+  $("#div-18").slideToggle();
+});
+$(function () {
+  $("#btn-18").on("click", function () {
+    if ($('p',this).hasClass("arrow")) {
+      $('p',this).removeClass("arrow").addClass("arrow-rotate");
+    } else if ($('p',this).hasClass("arrow-rotate")) {
+      $('p',this).removeClass("arrow-rotate").addClass("arrow");
+    }
+  });
+});
+
+$("#btn-19").on("click", function () {
+  $("#div-19").slideToggle();
+});
+$(function () {
+  $("#btn-19").on("click", function () {
+    if ($('p',this).hasClass("arrow")) {
+      $('p',this).removeClass("arrow").addClass("arrow-rotate");
+    } else if ($('p',this).hasClass("arrow-rotate")) {
+      $('p',this).removeClass("arrow-rotate").addClass("arrow");
+    }
+  });
+});
